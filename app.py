5c40dd509965d5756ae3876863cede5513f7a993
Tianle Chen <tianlechen@gmail.com>
2022-02-20 11:07:09 -0500

    cache

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
-from functools import lru_cache
-
 import streamlit as st
 from praw.models import Submission
 
 from interest import get_interest_rater
 from prawutils import get_reddit
+from sqlitedict import SqliteDict
 
 reddit = get_reddit()
 rater = get_interest_rater()
+interest_cache_path = 'cached_interest.sqlite'
 
 
-@lru_cache(maxsize=1000000)
 def get_interest(submission: Submission) -> float:
     s = f'''Title: {submission.title}
     Subreddit: {submission.subreddit.display_name}
@@ -20,8 +19,35 @@ def get_interest(submission: Submission) -> float:
     return rater(s[:1000])
 
 
-submissions = list(reddit.front.top(time_filter='day'))
-submissions.sort(key=get_interest, reverse=True)
+def get_interest_cached(submission: Submission) -> float:
+    with SqliteDict(interest_cache_path) as d:
+        res = d.get(submission.id, None)
+        if res is None:
+            res = get_interest(submission)
+            d[submission.id] = res
+            d.commit()
+    return res
+
+
+with st.sidebar:
+    subreddit = st.text_input('subreddit', value='all')
+
+
+submission_generator = (
+    reddit
+    .subreddit(display_name=subreddit)
+    .top(time_filter='day')
+)
+
+num_links_per_refresh = 25
+
+i = 0
+submissions = []
+while i < num_links_per_refresh:
+    submissions.append(next(submission_generator))
+    i += 1
+
+submissions = sorted(submissions, key=get_interest_cached, reverse=True)
 
 for i, submission in enumerate(submissions):
 
@@ -34,7 +60,7 @@ for i, submission in enumerate(submissions):
     [**{submission.title}**]({content_url})
 
     [{submission.num_comments} comments]({comments_url})
-    Interest: {get_interest(submission):.2f}
+    Interest: {get_interest_cached(submission):.2f}
     Score: {submission.score}
     Upvote ratio: {submission.upvote_ratio}
 

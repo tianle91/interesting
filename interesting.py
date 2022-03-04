3c3c81b5e83a12683324f1055194edb76fc3d775
Tianle Chen <tianlechen@gmail.com>
2022-03-03 21:44:05 -0500

    only requests

--- interesting.py
+++ interesting.py
@@ -1,28 +1,47 @@
+import asyncio
+import os
+from typing import Dict, List
+from urllib.parse import urljoin
+
+import requests
 from praw.models import Submission
 from sqlitedict import SqliteDict
 
-from interest import get_interest_rater
 from prawutils import get_reddit
 
 reddit = get_reddit()
-rater = get_interest_rater()
 interest_cache_path = 'cached_interest.sqlite'
 
+HF_INFERENCE_URL = os.getenv('HF_INFERENCE_URL')
 
-def get_interest(submission: Submission) -> float:
-    s = f'''Title: {submission.title}
+
+async def get_interest(submission: Submission) -> float:
+    body = f'''Title: {submission.title}
     Subreddit: {submission.subreddit.display_name}
     {submission.selftext}'''
     if len(submission.selftext) > 0 and submission.num_comments > 0:
-        s += f'\n{submission.comments[0].body}'
-    return rater(s[:1000])
+        body += f'\n{submission.comments[0].body}'
+    url = urljoin(HF_INFERENCE_URL, 'gpt2loss')
+    return float(requests.post(url, json=body[:1000]).json()['loss'])
 
 
-def get_interest_cached(submission: Submission) -> float:
+async def get_interests(submissions: List[Submission]) -> Dict[str, float]:
+    res = {}
     with SqliteDict(interest_cache_path) as d:
-        res = d.get(submission.id, None)
-        if res is None:
-            res = get_interest(submission)
-            d[submission.id] = res
-            d.commit()
+        new_submissions = []
+        for submission in submissions:
+            if submission.id in d:
+                res[submission.id] = d[submission.id]
+            else:
+                new_submissions.append(submission)
+
+    new_interests = await asyncio.gather(*[
+        get_interest(submission)
+        for submission in new_submissions
+    ])
+    with SqliteDict(interest_cache_path) as d:
+        for new_submission, new_interest in zip(new_submissions, new_interests):
+            d[new_submission.id] = new_interest
+            res[new_submission.id] = new_interest
+        d.commit()
     return res
